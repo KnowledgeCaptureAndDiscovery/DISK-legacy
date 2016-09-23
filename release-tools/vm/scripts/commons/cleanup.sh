02f73a5e9586863fddf88a73042c4272c453aeb4
Rajiv Mayani <mayani@isi.edu>
2016-09-23 16:16:57 -0700

    Stricter cleaning

--- release-tools/vm/scripts/commons/cleanup.sh
+++ release-tools/vm/scripts/commons/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm --recursive --force /root/*
+
+truncate --size 0 `find /var/log -type f | xargs`
 
 yum -y remove gcc kernel-devel `package-cleanup --leaves | grep -v 'Loaded plugins' | xargs`
 
