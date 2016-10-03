b125a2c8a1696f8a9d5dbc5fb167c7e07f305b22
Rajiv Mayani <mayani@isi.edu>
2016-10-03 16:25:10 -0700

    Use quiet mode of package-cleanup

--- release-tools/vm/scripts/commons/cleanup.sh
+++ release-tools/vm/scripts/commons/cleanup.sh
@@ -4,6 +4,8 @@ rm --recursive --force /root/*
 
 truncate --size 0 `find /var/log -type f | xargs`
 
-yum -y remove gcc kernel-devel `package-cleanup --leaves | grep -v 'Loaded plugins' | xargs`
+yum -y remove gcc kernel-devel `package-cleanup --leaves --quiet`
 
 yum clean all
+
+truncate --size 0 ~/.bash_history ; history -c
