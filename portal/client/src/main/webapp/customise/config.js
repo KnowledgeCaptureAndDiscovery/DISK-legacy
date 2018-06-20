91f6f70ddd6788ed5716a2d4d12e19e03e97c60e
Varun Ratnakar <varunratnakar@gmail.com>
2018-06-19 18:02:24 -0700

    Adding default config

---
+++ portal/client/src/main/webapp/customise/config.js
@@ -0,0 +1,21 @@
+var CONFIG = {
+	SERVER : "http://localhost:9090/disk-project-server",
+	TITLE : "Disk",
+	COLORS : {
+		base : "#5D7BA0", // Background color for Top Banner, and Headers
+		link : "#5D7BA0", // Text color for Links
+		header : "white", // Text color for Headers		
+		ok : "#5D7BA0", // Color indicating form items that are filled out
+		error : "#D42041" // Color indicating required form items that are not filled out 
+	}
+}
+
+CONFIG.WINGS = {
+	userid : "admin",
+	domain : "genomics"
+}
+
+CONFIG.HOME = "	<p>\n" + 
+		"		This portal provides access to the DISK Hypothesis reasoner\n" + 
+		"	</p>\n";
+
