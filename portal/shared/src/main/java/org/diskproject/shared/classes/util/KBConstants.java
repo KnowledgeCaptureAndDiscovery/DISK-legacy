f976e48a2f74eb3e17dbecb5ad21b1ff5b74bd7b
Hernán Vargas <hern.var@gmail.com>
2020-03-04 16:24:55 -0300

    Updated Enigma Hypothesis ontology to version 2.0.0

--- portal/shared/src/main/java/org/diskproject/shared/classes/util/KBConstants.java
+++ portal/shared/src/main/java/org/diskproject/shared/classes/util/KBConstants.java
@@ -2,7 +2,7 @@ package org.diskproject.shared.classes.util;
 
 public class KBConstants {
   private static String diskuri = "https://knowledgecaptureanddiscovery.github.io/DISK-Ontologies/disk/1.1.0/ontology.ttl";
-  private static String neurouri = "https://knowledgecaptureanddiscovery.github.io/DISK-Ontologies/enigma_hypothesis/release/1.1.0/ontology.ttl";
+  private static String neurouri = "https://knowledgecaptureanddiscovery.github.io/DISK-Ontologies/enigma_hypothesis/release/2.0.0/ontology.ttl";
   private static String omicsuri = "https://knowledgecaptureanddiscovery.github.io/DISK-Ontologies/omics_hypothesis/release/0.0.1/ontology.ttl";
   private static String hypuri = "https://knowledgecaptureanddiscovery.github.io/DISK-Ontologies/hypothesis/0.0.1/ontology.owl";
 
