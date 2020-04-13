65f10b55ac0c13db902102d2f9581978b1c8a5f6
Hernán Vargas <hern.var@gmail.com>
2020-04-13 09:26:03 -0400

    Changing version number

--- portal/client/src/main/java/org/diskproject/client/components/tloi/TriggeredLOIViewer.java
+++ portal/client/src/main/java/org/diskproject/client/components/tloi/TriggeredLOIViewer.java
@@ -1,7 +1,5 @@
 package org.diskproject.client.components.tloi;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.diskproject.client.Config;
